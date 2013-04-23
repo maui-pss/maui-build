4fa61bc44cd9652c14927592b881bd9709e772e9
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2013-04-23 20:01:19 +0200

    qa_make_disk: Rewrite using libqa
    Rewrite using libqa updates.
    
    Closes #23

--- src/ostbuild/pyostbuild/builtins/qa_make_disk.py
+++ src/ostbuild/pyostbuild/builtins/qa_make_disk.py
@@ -20,9 +20,7 @@
 import os, argparse, shutil
 
 from .. import builtins
-from ..subprocess_helpers import run_sync, run_sync_get_output
-from ..subprocess_helpers import run_sync_monitor_log_file
-from ..guestfish import GuestFish
+from .. import libqa
 
 class BuiltinQaMakeDisk(builtins.Builtin):
     name = "qa-make-disk"
@@ -40,53 +38,12 @@ class BuiltinQaMakeDisk(builtins.Builtin):
         if os.path.exists(path):
             raise Exception("Path %s already exist" % path)
 
-        tmppath = os.path.join(os.path.dirname(path), os.path.basename(path) + ".tmp")
-        if os.path.exists(tmppath):
-            if not os.path.isfile(tmppath):
-                raise Exception("%s already exist and is not a file, cannot delete it" % tmppath)
-            os.unlink(tmppath)
+        tmppath = path + ".tmp"
+        if not os.path.exists(tmppath):
+            shutil.rmtree(tmppath)
 
-        size_mb = 8 * 1024
-        bootsize_mb = 200
-        swapsize_mb = 64
-
-        run_sync(["qemu-img", "create", tmppath, "%sM" % size_mb])
-
-        make_disk_cmd = "launch\n" \
-            "part-init /dev/vda mbr\n" \
-            "blockdev-getsize64 /dev/vda\n" \
-            "blockdev-getss /dev/vda\n"
-        gf = GuestFish(tmppath, partition_opts=[], read_write=True)
-        lines = gf.run(make_disk_cmd).split("\n")
-        if len(lines) != 2:
-            raise Exception("guestfish returned unexpected output lines (%d), expected 2" % len(lines))
-
-        disk_bytesize = int(lines[0])
-        disk_sectorsize = int(lines[1])
-        self.logger.info("bytesize: %d, sectorsize: %d" % (disk_bytesize, disk_sectorsize))
-
-        bootsize_sectors = bootsize_mb * 1024 / disk_sectorsize * 1024
-        swapsize_sectors = swapsize_mb * 1024 / disk_sectorsize * 1024
-        rootsize_sectors = disk_bytesize / disk_sectorsize - bootsize_sectors - swapsize_sectors - 64
-        boot_offset = 64
-        swap_offset = boot_offset + bootsize_sectors
-        root_offset = swap_offset + swapsize_sectors
-        end_offset = root_offset + rootsize_sectors
-
-        partconfig = "launch\n" \
-            "part-add /dev/vda p %s %s\n" \
-            "part-add /dev/vda p %s %s\n" \
-            "part-add /dev/vda p %s %s\n" \
-            "mkfs ext4 /dev/vda1\n" \
-            "set-e2label /dev/vda1 maui-boot\n" \
-            "mkswap-L maui-swap /dev/vda2\n" \
-            "mkfs ext4 /dev/vda3\n" \
-            "set-e2label /dev/vda3 maui-root\n" \
-            "mount /dev/vda3 /\n" \
-            "mkdir /boot\n" % (boot_offset, swap_offset - 1, swap_offset,
-                               root_offset - 1, root_offset, end_offset - 1)
-        self.logger.info("partition configuration: %s" % partconfig)
-        lines = gf.run(partconfig).rstrip().split('\n')
+        # FIXME: Get osname from snapshot
+        libqa.create_disk(tmppath, "maui")
         os.rename(tmppath, path)
         self.logger.info("Created: %s" % path)
 
